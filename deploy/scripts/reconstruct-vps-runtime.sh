2657b8336ebe2456cb361bdbc00705dcaf7d550c
Kayhan <kayhankyp@gmail.com>
2026-05-31 15:09:50 +0300

    Route root Next assets through nginx

--- deploy/scripts/reconstruct-vps-runtime.sh
+++ deploy/scripts/reconstruct-vps-runtime.sh
@@ -858,6 +858,17 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
+    location ^~ /_next/ {
+        proxy_pass http://127.0.0.1:${ROOT_PORT};
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
     location = /adisyonsistemi {
         return 308 /app;
     }
